6b6bfc0730c95f5b7bb81b389fff0b3686008fba
ohboh <myrrhgh@Myrrhs-MacBook-Air.local>
2022-10-26 23:40:57 +0800

    Add UI

--- rps.js
+++ rps.js
@@ -1,9 +1,49 @@
 
 var playerScore = 0;
 var computerScore = 0;
-var computerSelection;
+var computerSelection = "";
+var playerSelection = "";
+const playerScoreShow = document.getElementById("player");
+const computerScoreShow = document.getElementById("computer");
+const winnerShow = document.getElementById("winner");
+
+document.querySelectorAll("button").forEach(occurence => {
+    let id = occurence.getAttribute("id")
+    occurence.addEventListener("click", function(){
+        playerSelection = id;
+
+        if (playerScore < 5 && computerScore <5 ){
+            playRound();
+            playerScoreShow.textContent = ("player: " + playerScore.toString());
+            computerScoreShow.textContent = ("computer: " + computerScore.toString());
+
+            if (playerScore == 5){
+                winnerShow.textContent = ("y-y-y-y-you won?!! ヽ(°〇°)ﾉ")
+            }
+    
+            else if (computerScore == 5){
+                winnerShow.textContent = ("I WON!!! <(￣︶￣)>")
+            }
+
+            if (playerScore == 4 && computerScore < 4){
+                winnerShow.textContent = ("(⊙.⊙)")
+            }
+
+            if (computerScore == 4 && playerScore < 4){
+                winnerShow.textContent = ("ψ(｀∇´)ψ")
+            }
+
+            else if (playerScore == 4 && computerScore == 4){
+                winnerShow.textContent = ("(｡ŏ_ŏ)")
+            }
+
+
+        }
+    });
+});
 
 function playRound() {
+
     computerSelection = getComputerChoice();
     if (playerSelection == "rock"){
 
@@ -45,26 +85,19 @@ function getComputerChoice(){
 }
 
 function game(){
-    for(let i = 0; i < 5; i++){
-        playerSelection = prompt("rock, paper, scissors? ").toLowerCase();
-        playRound();;
-        console.log ("Player: " + playerScore.toString());
-        console.log ("Computer: " + computerScore.toString());
-        if (i == 4) {
-            if (playerScore > computerScore){
-                console.log("YOU WON!!!");
-            }
-            else if (playerScore < computerScore){
-                console.log("YOU LOST!!! MUAHAHAHAHAHA");
-            }
-            else if (playerScore == computerScore){
-                console.log("IT'S A TIE!!!! BRUHHHHH");
-            }
-            else {
-                console.log("how?");
-            }
-        }
-    }
+    playRound();
+    console.log ("Player: " + playerScore.toString());
+    console.log ("Computer: " + computerScore.toString());
+            //if (playerScore > computerScore){
+            //    console.log("YOU WON!!!");
+            //}
+            //else if (playerScore < computerScore){
+            //    console.log("YOU LOST!!! MUAHAHAHAHAHA");
+            //}
+            //else if (playerScore == computerScore){
+            //    console.log("IT'S A TIE!!!! BRUHHHHH");
+            //}
+            //else {
+            //   console.log("how?");
+            //}
 }
-
-console.log(game());
